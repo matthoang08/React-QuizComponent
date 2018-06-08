f264353fab7c2f2e9e88c366b77de86bf453826c
Matt Hoang <matt.hoang@coxautoinc.com>
2018-06-08 16:40:03 -0500

    added material UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react'
 import './App.css'
 import Quiz from './quiz/Quiz.js'
 import Chat from './chat/chat'
+import Grid from '@material-ui/core/Grid'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Quiz />
-        <Chat />
+        <Grid container>
+          <Grid item xs={4}>test</Grid>
+          <Grid item xs={8}>
+            <Quiz />
+            <Chat />
+          </Grid>
+        </Grid>
       </div>
     )
   }
